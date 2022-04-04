118b9850550c4642c56ac6c9e0f733be7ab6566c
Federico Barboza <fede.barboza@icloud.com>
2022-04-03 21:07:57 -0300

    Homework 06 Algoritmos I

--- 06-Algoritmos-I/homework/homework.js
+++ 06-Algoritmos-I/homework/homework.js
@@ -6,7 +6,17 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  var array = [1];
+  var fact = 2;
+    while(num > 1){
+        if(num % fact === 0){
+            array.push(fact);
+            num = num / fact;
+        }else {
+            fact++
+        }
+    }
+    return array;
 }
 
 function bubbleSort(array) {
@@ -14,7 +24,15 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+        if(array[j] > array[j + 1]){
+            var temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+  } return array;
 }
 
 
@@ -23,7 +41,15 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (var i = 1; i < array.length; i++) {
+    for (var j = i; j > 0; j--){
+      if(array[j] < array[j - 1]){
+          var temp = array[j];
+          array[j] = array[j - 1];
+          array[j - 1] = temp;
+      }
+    }
+  } return array;
 }
 
 
@@ -32,7 +58,16 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (var i = 0; i < array.length - 1; i++) {
+    var minIndex = i;
+    for (var j = i + 1; j < array.length; j++){
+      if(array[j] < array[minIndex]){
+          minIndex = j;
+      }  
+    } var temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+} return array;
 }
 
 
